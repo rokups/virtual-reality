6d39d15b8be187bcc246ab2c7613fb037e8a1ce7
Rokas Kupstys <rokups@zoho.com>
2019-11-07 14:57:54 +0200

    Global WIN32_LEAN_AND_MEAN definition.

--- src/shared/ReflectiveLoader.h
+++ src/shared/ReflectiveLoader.h
@@ -28,7 +28,9 @@
 #ifndef _REFLECTIVEDLLINJECTION_REFLECTIVELOADER_H
 #define _REFLECTIVEDLLINJECTION_REFLECTIVELOADER_H
 //===============================================================================================//
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
 #include <intrin.h>
 
